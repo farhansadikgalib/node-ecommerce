373b9a3a10d2f21cb0e70b398a897ac56f39e9ff
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-08-03 14:12:52 +0600

    Prepare for Vercel deployment: update auth responses and deployment config

--- controllers/authController.js
+++ controllers/authController.js
@@ -424,6 +424,7 @@ exports.login = async (req, res) => {
     const token = generateToken(user._id);
 
     res.status(200).json({
+      success: true,
       message: "Login successful",
       token,
       user: {
